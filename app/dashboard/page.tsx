a44b7e08f2da474cc8fd1ae1507adc3d3862b59d
Shivam Thakur <shivamth060@gmail.com>
2024-05-06 17:37:35 +0530

    Complete

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,39 @@
+import CompanionFeed from "@/components/CompanionFeed";
+import { prisma } from "@/prisma/client";
+import React from "react";
+
+interface DashboardProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+const DashboardPage = async ({ searchParams }: DashboardProps) => {
+  const data = await prisma.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <CompanionFeed data={data} />
+    </>
+  );
+};
+
+export default DashboardPage;
